69786ed0b035a3a0503ccb7feaf3713b647922a7
patrickbindelli <patrickvenanbindelli@gmail.com>
2024-06-06 22:53:17 -0300

    add some shortcuts

--- config.def.h
+++ config.def.h
@@ -1,5 +1,7 @@
 /* See LICENSE file for copyright and license details. */
 
+#include <X11/XF86keysym.h>
+
 /* appearance */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int gappx     = 6;        /* gaps between windows */
@@ -65,6 +67,16 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
+static const char *lockcmd[]  = { "slock", NULL};
+
+/* volume control */
+static const char *upvol[] = { "/usr/bin/pactl", "set-sink-volume", "@DEFAULT_SINK@", "+5%", NULL };
+static const char *downvol[] = { "/usr/bin/pactl", "set-sink-volume", "@DEFAULT_SINK@", "-5%", NULL };
+static const char *mutevol[] = { "/usr/bin/pactl", "set-sink-mute", "@DEFAULT_SINK@", "toggle", NULL };
+
+/* light control */
+static const char *light_up[]   = { "xbacklight",   "-inc", "5", NULL };
+static const char *light_down[] = { "xbacklight",   "-dec", "5", NULL };
 
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
@@ -101,6 +113,12 @@ static const Key keys[] = {
 	TAGKEYS(                        XK_8,                      7)
 	TAGKEYS(                        XK_9,                      8)
 	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
+	{ MODKEY|ShiftMask,             XK_l,      spawn,          {.v = lockcmd} },
+	{ 0,                       XF86XK_AudioRaiseVolume,        spawn,          {.v = upvol } },
+	{ 0,                       XF86XK_AudioLowerVolume,        spawn,          {.v = downvol } },
+	{ 0,                       XF86XK_AudioMute,               spawn,          {.v = mutevol } },
+	{ 0,				       XF86XK_MonBrightnessUp,		   spawn,	       {.v = light_up} },
+	{ 0,				       XF86XK_MonBrightnessDown,	   spawn,	       {.v = light_down} },
 };
 
 /* button definitions */
